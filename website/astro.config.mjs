5b4592b9751a0ae2045e336b608cf615de8a055d
Nebula <Nebulawastaken.dev@proton.me>
2026-09-13 21:23:43 +0300

    Delete website directory

--- website/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  output: 'static',
-  integrations: [react(), tailwind({ applyBaseStyles: false })],
-  vite: { build: { cssCodeSplit: true } }
-});
